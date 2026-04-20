6427afc2d89941d231baa272845280fbfb46b0c0
raimonvibe <robertjanstefan@gmail.com>
2026-04-20 19:02:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import PrayerChatWidget from '@/PrayerChatbot';
+import PrayerChatWidget from '../components/PrayerChatWidget'
 
 export const metadata: Metadata = {
   title: 'Holy Bible Reader - Old & New Testament',
